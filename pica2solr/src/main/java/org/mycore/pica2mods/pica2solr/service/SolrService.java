9fdd3bc35f7a1cb1ceecb1adfd76153b8b48b31e
Robert Stephan <robert.stephan@uni-rostock.de>
2023-10-24 10:57:00 +0200

    update dependencies / cleanup code #87

--- pica2solr/src/main/java/org/mycore/pica2mods/pica2solr/service/SolrService.java
+++ pica2solr/src/main/java/org/mycore/pica2mods/pica2solr/service/SolrService.java
@@ -8,8 +8,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import javax.json.Json;
